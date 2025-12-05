3bb4bf431e0ffd88d67e8926aaba3db5102ab986
IsaiasCuvula <bersyteinfo@gmail.com>
2025-12-05 23:04:34 +0200

    get getNextRank

--- src/models/Rank.java
+++ src/models/Rank.java
@@ -10,6 +10,20 @@ public enum Rank {
     BATTLE_MASTER,
     GRAND_MASTER;
 
+
+    public Rank getNextRank(){
+        try{
+            //The GRAND_MASTER is the last in this case
+            if(this == Rank.GRAND_MASTER){
+               return this;
+            }
+            int actualIndex = this.ordinal();
+            return Rank.values()[actualIndex + 1];
+        }catch (Exception e){
+            return this;
+        }
+    }
+
     public static Rank fromString(String s){
         try{
             if(s == null) return null;
