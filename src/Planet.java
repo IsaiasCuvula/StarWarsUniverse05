fb14a60bb141ce9f2b11c2f767c154be110cd2e4
IsaiasCuvula <bersyteinfo@gmail.com>
2025-12-05 20:47:14 +0200

    get Jedi List and add Jedi to the list

--- src/Planet.java
+++ src/Planet.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Planet {
     private String name;
-    private final List<Jedi> inhabitants = new ArrayList<>();
+    private final List<Jedi> jediList = new ArrayList<>();
 
     public Planet(String name) {
         this.name = name;
@@ -11,6 +11,17 @@ public class Planet {
 
     public String getName() {
         return name;
+    }
+
+    public List<Jedi> getJediList() {
+        return jediList;
+    }
+
+    public void addJedi(Jedi jedi) {
+        if(jedi == null){
+             throw new IllegalArgumentException("Jedi cannot be null");
+        }
+        jediList.add(jedi);
     }
 
     public void setName(String name) {
