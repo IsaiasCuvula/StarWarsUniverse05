f98a1b1c43479f707fa1b119ee1af23f5bd4956e
IsaiasCuvula <bersyteinfo@gmail.com>
2025-12-06 10:51:50 +0200

    added step down rank method

--- src/models/Rank.java
+++ src/models/Rank.java
@@ -10,8 +10,20 @@ public enum Rank {
     BATTLE_MASTER,
     GRAND_MASTER;
 
+    public Rank stepDownRank(){
+        try{
+            //The YOUN GLING is the last in this case
+            if(this == Rank.YOUNGLING){
+                return this;
+            }
+            int actualIndex = this.ordinal();
+            return Rank.values()[actualIndex - 1];
+        }catch (Exception e){
+            return this;
+        }
+    }
 
-    public Rank getNextRank(){
+    public Rank stepUpRank(){
         try{
             //The GRAND_MASTER is the last in this case
             if(this == Rank.GRAND_MASTER){
