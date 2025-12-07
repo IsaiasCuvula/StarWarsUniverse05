9ca29ae1feef145ec2468faf645612e3133158ff
IsaiasCuvula <bersyteinfo@gmail.com>
2025-12-07 20:43:36 +0200

    setStrength

--- src/Application.java
+++ src/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         FileManager fileManager = new FileManager();
-        fileManager.open("stars_war.text");
+        fileManager.open("stars_wars.text");
         StarWarsUniverse starWarsUniverse = new StarWarsUniverse();
         ApplicationManager manager = new ApplicationManager(fileManager, starWarsUniverse);
         Scanner scanner = new Scanner(System.in);
